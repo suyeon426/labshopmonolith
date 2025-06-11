6791d76a909cab1a48be1b0c48ee509c47d7ed12
suyeon426 <145529554+suyeon426@users.noreply.github.com>
2025-06-11 14:38:12 +0900

    commit

---
+++ frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+        <VerticalNavSectionTitle :item="{ heading: 'Monolith' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Order',
+                to: '/orders',
+            }"
+        />
+        <VerticalNavLink
+            :item="{
+                title: 'Inventory',
+                to: '/inventories',
+            }"
+        />
+    </ul>
+</template>
---
+++ monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -0,0 +1,45 @@
+package labshopmonolith.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import labshopmonolith.MonolithApplication;
+import labshopmonolith.domain.OrderPlaced;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String customerId;
+
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
